3af1ff5c3d62b4c82a8cef9d4d5dff2f8c06bd4f
reham.rashid <reham.rashid@gds-direct.com>
2023-12-26 21:36:07 +0200

    meals done

--- assets/dialogTitleClasses-b98083ca.js
+++
@@ -1,1 +0,0 @@
-import{bx as i,bw as s}from"./index-8dd84c3c.js";function a(t){return s("MuiDialogTitle",t)}const l=i("MuiDialogTitle",["root"]),o=l;export{o as d,a as g};
--- assets/use-params-25c51b5a.js
+++
@@ -1,1 +0,0 @@
-import{da as r,r as s}from"./index-8dd84c3c.js";function o(){const a=r();return s.useMemo(()=>a,[a])}export{o as u};
